807b13d9db6b6ee489268adaedc5c07f31575bdd
lebron <595452769@qq.com>
2023-11-30 02:36:47 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -11,5 +12,18 @@ class UsersController extends Controller
     public function show(User $user)
     {
         return view('users.show', compact('user'));
+    }
+
+    // 编辑个人资料
+    public function edit(User $user)
+    {
+        return view('users.edit', compact('user'));
+    }
+
+    // 处理编辑个人资料
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
