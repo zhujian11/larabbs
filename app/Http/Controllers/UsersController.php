42a62d7b0352295611a4b88fd95602c72e0c6975
lebron <595452769@qq.com>
2023-11-30 06:19:58 +0000

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -9,6 +9,11 @@ use App\Handlers\ImageUploadHandler;
 
 class UsersController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth', [
+            'except' => ['show']
+        ]);
+    }
     // 展示个人页面
     public function show(User $user)
     {
@@ -18,12 +23,15 @@ class UsersController extends Controller
     // 编辑个人资料
     public function edit(User $user)
     {
+        $this->authorize('update', $user);
         return view('users.edit', compact('user'));
     }
 
     // 处理编辑个人资料
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user)
     {
+        $this->authorize('update', $user);
+
         $data = $request->all();
 
         if ($request->avatar) {
