eabbc2859841cd004f8fd21a93b8f605aecdf72b
zhujian11 <595452769@qq.com>
2023-11-30 16:25:28 +0800

    分类下的话题列表

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,11 @@ Route::get('email/verify', 'Auth\VerificationController@show')->name('verificati
 Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
 Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
 
+// 用户
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 
+// 话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
