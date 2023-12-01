b4011e182d6b4dcfb1499a12207dfeab1a678962
zhujian11 <595452769@qq.com>
2023-12-01 11:45:54 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -46,5 +46,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // 话题
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 
+// 上传图片
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
+
 // 分类
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
