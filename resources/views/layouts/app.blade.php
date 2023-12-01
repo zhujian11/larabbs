612778c27c9e6c11abf5b56d6c29b75fc11ff85b
zhujian11 <595452769@qq.com>
2023-12-01 12:03:34 +0800

    话题内容页面

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,8 @@
 
   <title>@yield('title', 'LaraBBS') - Laravel 进阶课程</title>
 
+  <meta name="description" content="@yield('description', 'LaraBBS 爱好者社区')">
+
   <!-- Styles -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   @yield('styles')
