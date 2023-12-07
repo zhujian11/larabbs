ac3825f48937746d7fdd680e4ba97529c7f14faa
zhujian11 <595452769@qq.com>
2023-12-07 12:15:54 +0800

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -59,3 +59,6 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 无权限
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
